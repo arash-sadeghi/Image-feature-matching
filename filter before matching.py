3de060a9b690a52fa9c6633ff317a560c12f61d1
arash-sadeghi <arash.sadeghi1997@gmail.com>
2021-09-23 22:42:43 +0200

    pre and post filter completed without any bug

--- filter before matching.py
+++ filter before matching.py
@@ -9,6 +9,36 @@ from time import ctime,time
 MAX_FEATURES = 500
 GOOD_MATCH_PERCENT = 0.5
 RESULTS_FOLDER = 'results '+ctime(time()).replace(':','_')
+BAND=[-30,30]
+distance_thresh=60
+# -----------------------------------------------------------------------------------------------
+def calculate_homography(im1, keypoints1, im2, keypoints2, matches):
+        # Extract location of good matches
+        points1 = np.zeros((len(matches), 2), dtype=np.float32)
+        points2 = np.zeros((len(matches), 2), dtype=np.float32)
+        
+        for i, match in enumerate(matches):
+            points1[i, :] = keypoints1[match.queryIdx].pt
+            points2[i, :] = keypoints2[match.trainIdx].pt
+        
+        # Find homography
+        h, mask = cv.findHomography(points1, points2, cv.RANSAC)
+        
+        # Use homography
+        height, width, channels = im2.shape
+        im1Reg = cv.warpPerspective(im1, h, (width, height))
+        return im1Reg, h
+# -----------------------------------------------------------------------------------------------
+def draw_results(im1, keypoints1, im2, keypoints2, matches,method):
+    imMatches,displacements = my_draw_matches(im1, keypoints1, im2, keypoints2, matches,None)
+    cv.imwrite(RESULTS_FOLDER+'/'+method+'_matches'+'.jpg', imMatches)
+    hist_my=np.histogram(displacements[:,0])
+    x_displacement=hist_my[1][np.argmax(hist_my[0])]
+    plt.clf() 
+    plt.xlim(-600,600) #! cheat
+    plt.hist(displacements[:,0],bins=100)
+    plt.savefig(RESULTS_FOLDER+'/'+method+'_hist.png')
+    print('[+] method {} x displacement is {:.2f}'.format(method,x_displacement))
 # -----------------------------------------------------------------------------------------------
 def read_dataset(location=None,dataset_folder_name='GRIEF-datasets',subfolder='michigan',im_extention='bmp'):
     if location is None and subfolder is None:
@@ -53,32 +83,14 @@ def random_color():
     return (randint(0,255),randint(0,255),randint(0,255))
 # -----------------------------------------------------------------------------------------------
 def my_draw_matches(im1, keypoints1, im2, keypoints2, matches,_):    
-    # canvas=np.zeros((max(im1.shape[0],im2.shape[0]),im1.shape[1]+im2.shape[1],3))
-    # canvas[0:im1.shape[0],0:im1.shape[1]]=im1
-    # canvas[0:im2.shape[0] , im1.shape[1]:im1.shape[1]+im2.shape[1]]=im2
-
     canvas=np.zeros((im1.shape[0]+im2.shape[0],max(im1.shape[1],im2.shape[1]),3))
     canvas[0:im1.shape[0],0:im1.shape[1]]=im1
     canvas[im1.shape[0]:im1.shape[0]+im2.shape[0],0:im2.shape[1]]=im2
-    # cv.imshow('canvas',canvas)
-    # cv.waitKey()
-    cv.imwrite(RESULTS_FOLDER+'/'+'canvas.jpg',canvas)
-
     displacements=np.zeros((len(keypoints1),2))
-    # for i in range(len(keypoints1)):
-    #     p1=(int(keypoints1[i].pt[0]),int(keypoints1[i].pt[1]))
-    #     # p2=(int(keypoints2[i].pt[0]+im2.shape[1]),int(keypoints2[i].pt[1]))
-    #     p2=(int(keypoints2[i].pt[0]),int(keypoints2[i].pt[1]+im2.shape[0]))
-    #     displacements[i,0]=p2[0]-p1[0]
-    #     displacements[i,1]=p2[1]-p1[1]
-    #     color=random_color()
-    #     cv.circle(canvas,p1,10,color,-1)
-    #     cv.circle(canvas,p2,10,color,-1)
-    #     cv.line(canvas, p1, p2, color, thickness=5)
-
+    # cv.circle(canvas,(200,200),30,(0,255,0),-1) #! for debug purpose
     for i in range(len(matches)):
-        p1=(int(keypoints1[matches[i].queryIdx].pt[0]),int(keypoints1[matches[i].queryIdx].pt[1]))
-        p2=(int(keypoints2[matches[i].trainIdx].pt[0]),int(keypoints2[matches[i].trainIdx].pt[1]+im2.shape[0]))
+        p1=(int( keypoints1[matches[i].queryIdx].pt[0] ),int(keypoints1[matches[i].queryIdx].pt[1]))
+        p2=(int( keypoints2[matches[i].trainIdx].pt[0] ),int(keypoints2[matches[i].trainIdx].pt[1]+im2.shape[0]))
         displacements[i,0]=p2[0]-p1[0]
         displacements[i,1]=p2[1]-p1[1]
         color=random_color()
@@ -87,6 +99,37 @@ def my_draw_matches(im1, keypoints1, im2, keypoints2, matches,_):
         cv.line(canvas, p1, p2, color, thickness=5)
 
     return canvas,displacements
+# -----------------------------------------------------------------------------------------------
+def pre_filter_match(keypoints1, descriptors1, keypoints2 ,descriptors2,matcher,band):
+    #! time consuming part
+    #! mathcer only acts on uint8 data type zz=np.zeros((5, len(descriptors1[c]) ),dtype=np.uint8)
+    match_obj=matcher.match(descriptors1,descriptors2,None)
+    counter=0
+    for c,v in enumerate(keypoints1):
+        nominee=[]
+        nominee_indx=[]
+        for cc,vv in enumerate(keypoints2):
+            if band[0]<=v.pt[0]-vv.pt[0]<=band[1]:
+                # arg1=np.zeros((1,))
+                nominee.append(descriptors2[cc])
+                nominee_indx.append(cc)
+        if len(nominee)==0: # no match found
+            continue
+        nominee=np.array(nominee)
+        tricker=np.zeros( nominee.shape , dtype=np.uint8 )
+        tricker[:]=descriptors1[c]
+        #! [0] below is because all trainIdx are the same
+        # matches_indx=matcher.match(tricker,nominee,None)[0]
+        matched=matcher.match(tricker,nominee,None)[0]
+        if matched.distance>=distance_thresh:
+            continue
+        else:
+            matches_indx=matched.trainIdx
+        match_obj[counter].queryIdx=c
+        match_obj[counter].trainIdx=nominee_indx[matches_indx]
+        counter+=1
+
+    return match_obj[0:counter+1]
 # -----------------------------------------------------------------------------------------------
 def alignImages(im1, im2,band=None,method='opencv'):
     
@@ -99,86 +142,64 @@ def alignImages(im1, im2,band=None,method='opencv'):
     orb = cv.ORB_create(MAX_FEATURES)
     keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
     keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+    #! ORB only creates the features. Matching is different process
+
     matcher = cv.DescriptorMatcher_create(cv.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
 
-    # Match features.
-    matches = matcher.match(descriptors1, descriptors2, None)
-    
+    if method=='opencv' or method=='post_filter':
+        # Match features.
+        matches = matcher.match(descriptors1, descriptors2, None)
+
+    elif method=='pre_filter':
+        matches = pre_filter_match(keypoints1, descriptors1, keypoints2 ,descriptors2,matcher,band)
+
     # Sort matches by score
     matches.sort(key=lambda x: x.distance, reverse=False)
-    
-    # # Remove not so good matches
+
+    # Remove not so good matches
     # numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
     # matches = matches[:numGoodMatches]
-    
+    distances=np.array([matches[_].distance for _ in range(len(matches))])
+    matches=matches[0:np.where(distances>distance_thresh)[0][0]]    
+
+
     # Draw top matches
-    imMatches = cv.drawMatches(im1, keypoints1, im2, keypoints2, matches,None)
-    cv.imwrite(RESULTS_FOLDER+'/'+'opencv_matches'+'.jpg', imMatches)
-    
-    if method=='mine':
-        imMatches_my,displacements_my = my_draw_matches(im1, keypoints1, im2, keypoints2, matches,None)
-        cv.imwrite(RESULTS_FOLDER+'/'+'my_matches'+'.jpg', imMatches_my)
-        hist_my=np.histogram(displacements_my[:,0])
-        x_displacement=hist_my[1][np.argmax(hist_my[0])]
-        plt.clf() 
-        plt.xlim(-600,600) #! cheat
-        plt.hist(displacements_my[:,0],bins=100)
-        plt.savefig(RESULTS_FOLDER+'/'+'my_original_hist.png')
-        print('[+] my x displacement is {:.2f}'.format(x_displacement))
-        # print('[+] x displacement range [{:.2f} , {:.2f}]'.format(x_displacement+band[0],x_displacement+band[1]))
-        
+    # imMatches = cv.drawMatches(im1, keypoints1, im2, keypoints2, matches,None)
+    # cv.imwrite(RESULTS_FOLDER+'/'+'opencv_matches'+'.jpg', imMatches)
+
+
+    if method=='post_filter':
         #! post filtering
-        if not band is None:    
-            filtered_matches=[]
-            for i in range(len(matches)):
-                if band[0]<matches[i].distance<band[1]:
-                    filtered_matches.append(matches[i])
-            imMatches_filtered,displacements_filtered = my_draw_matches(im1, keypoints1, im2, keypoints2, filtered_matches,None)
+        if band is None:    
+            raise NameError('[-] no band given for post_filter')
 
-            cv.imwrite(RESULTS_FOLDER+'/'+'matches_post_filtered'+'.jpg', imMatches_filtered)
-            hist_filtered=np.histogram(displacements_filtered[:,0])
-            x_displacement=hist_filtered[1][np.argmax(hist_filtered[0])]
-            plt.clf() 
-            plt.xlim(-600,600) #! cheat
-            plt.hist(displacements_filtered[:,0],bins=100)
-            plt.savefig(RESULTS_FOLDER+'/'+'post_filtered_hist.png')
-        # Extract location of good matches --------------------------------------
-        points1 = np.zeros((len(filtered_matches), 2), dtype=np.float32)
-        points2 = np.zeros((len(filtered_matches), 2), dtype=np.float32)
-        
-        for i, match in enumerate(filtered_matches):
-            points1[i, :] = keypoints1[match.queryIdx].pt
-            points2[i, :] = keypoints2[match.trainIdx].pt
-        
-        # Find homography
-        h_filtered, mask = cv.findHomography(points1, points2, cv.RANSAC)
-        
-        # Use homography
-        height, width, channels = im2.shape
-        im1Reg_filtered = cv.warpPerspective(im1, h_filtered, (width, height))
-
-        print('[+] post filtered x displacement within bands of {} is {:.2f}'.format(band,x_displacement))
-    
-        return im1Reg_filtered, h_filtered
-
+        filtered_matches=[]
+        for i in range(len(matches)):
+            #! norm2 distance
+            # dist=np.linalg.norm(np.array(keypoints1[matches[i].queryIdx].pt)-np.array(keypoints2[matches[i].trainIdx].pt))
+            
+            #! 1D distance
+            dist=keypoints1[matches[i].queryIdx].pt[0]-keypoints2[matches[i].trainIdx].pt[0]
+            #!? why some keypoint locations has floating parts arent they coordiantes
+            if band[0]<=dist<=band[1]:
+                filtered_matches.append(matches[i])
+        draw_results(im1, keypoints1, im2, keypoints2, filtered_matches,method)
+        # im1Reg_filtered, h_filtered=calculate_homography(im1, keypoints1, im2, keypoints2, filtered_matches)
+        # return im1Reg_filtered, h_filtered
+        return None,None
+    elif method=='pre_filter':
+        draw_results(im1, keypoints1, im2, keypoints2, matches,method)
+        # im1Reg, h=calculate_homography(im1, keypoints1, im2, keypoints2, matches)
+        # return im1Reg, h
+        return None,None
     elif method=='opencv':
-        # Extract location of good matches
-        points1 = np.zeros((len(matches), 2), dtype=np.float32)
-        points2 = np.zeros((len(matches), 2), dtype=np.float32)
-        
-        for i, match in enumerate(matches):
-            points1[i, :] = keypoints1[match.queryIdx].pt
-            points2[i, :] = keypoints2[match.trainIdx].pt
-        
-        # Find homography
-        h, mask = cv.findHomography(points1, points2, cv.RANSAC)
-        
-        # Use homography
-        height, width, channels = im2.shape
-        im1Reg = cv.warpPerspective(im1, h, (width, height))
-        
+        # draw_results(im1, keypoints1, im2, keypoints2, matches,method)
+        imMatches = cv.drawMatches(im1, keypoints1, im2, keypoints2, matches,None)
+        cv.imwrite(RESULTS_FOLDER+'/'+'opencv_matches'+'.jpg', imMatches)
+        # im1Reg, h=calculate_homography(im1, keypoints1, im2, keypoints2, matches)
+        # return im1Reg, h
+        return None,None
 
-        return im1Reg, h
 # -----------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
@@ -195,16 +216,19 @@ if __name__ == '__main__':
 
     truth=all_files[1,0,:]
 
-    print('ji')
-
-    # Registered image will be resotred in imReg.
-    # The estimated homography will be stored in h.
+    """Registered image will be resotred in imReg."""
+    """The estimated homography will be stored in h."""
+    
     imReg, h = alignImages(im1,im2,method='opencv')
-    imReg_post_filtered, h_post_filtered = alignImages(im1,im2,band=[-100,100],method='mine')
+    
+    imReg_post_filtered, h_post_filtered = alignImages(im1,im2,band=BAND,method='post_filter')
+
+    imReg_post_filtered, h_post_filtered = alignImages(im1,im2,band=BAND,method='pre_filter')
+
 
     print('[+] Truth is {}'.format(truth))
     # Write aligned image to disk.
-    cv.imwrite(RESULTS_FOLDER+'/'+"aligned.jpg", imReg)
-    cv.imwrite(RESULTS_FOLDER+'/'+"aligned_filtered.jpg", imReg_post_filtered)
+    # cv.imwrite(RESULTS_FOLDER+'/'+"aligned.jpg", imReg)
+    # cv.imwrite(RESULTS_FOLDER+'/'+"aligned_filtered.jpg", imReg_post_filtered)
     
     # Print estimated homography
--- tester.py
+++ tester.py
@@ -1,5 +1,7 @@
 import os
+import numpy as np
 print(os.listdir('GRIEF-datasets/michigan'))
 
 print(os.path.splitext('GRIEF-datasets/michigan/season_00/000000000.bmp'))
+print(np.dist([0,0],[1,1]))
   
